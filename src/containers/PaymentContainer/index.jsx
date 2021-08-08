25024faa85489b965c75a37136a4c462f8fc0ad1
wfercanas <fernando.canas@outlook.com>
2021-08-07 20:14:57 -0500

    add/ Payment mockup

--- src/containers/PaymentContainer/index.jsx
+++ src/containers/PaymentContainer/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import '../../styles/components/Payment.css';
 
 const PaymentContainer = () => {
-  return;
+  return (
+    <div className="Payment">
+      <div className="Payment-content">
+        <h3>Resumen del pedido:</h3>
+        <div className="Payment-button">Paga con PayPal</div>
+      </div>
+      <div />
+    </div>
+  );
 };
 
 export { PaymentContainer };
