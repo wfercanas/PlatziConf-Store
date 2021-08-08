812feb29f43ee05c586c218510ef0b50cae4c62a
wfercanas <fernando.canas@outlook.com>
2021-08-07 19:39:42 -0500

    add/ add basic Header and Footer Layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../styles/components/Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <p className="Footer-title">PlatziConf Store</p>
+      <p className="Footer-copy">Todos los derechos reservados</p>
+    </div>
+  );
+};
+
+export { Footer };
