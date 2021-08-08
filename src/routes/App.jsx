14dd8e4ed24f67c622fdf14692de4c727a5d1bd2
wfercanas <fernando.canas@outlook.com>
2021-08-07 19:39:01 -0500

    add/ wrap routes within header and footer

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import { BrowserRouter, Switch, Router } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Layout } from '../components/Layout';
 import { CheckoutContainer } from '../containers/CheckoutContainer';
 import { HomeContainer } from '../containers/HomeContainer';
 import { InformationContainer } from '../containers/InformationContainer';
 import { PaymentContainer } from '../containers/PaymentContainer';
 import { SuccessContainer } from '../containers/SuccessContainer';
+import { NotFound } from '../containers/NotFound';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={HomeContainer} />
-        <Route exact path="/checkout" component={CheckoutContainer} />
-        <Route
-          exact
-          path="/checkout/information"
-          component={InformationContainer}
-        />
-        <Route exact path="/checkout/payment" component={PaymentContainer} />
-        <Route exact path="/checkout/success" component={SuccessContainer} />
-        <Route component={NotFound} />
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={HomeContainer} />
+          <Route exact path="/checkout" component={CheckoutContainer} />
+          <Route
+            exact
+            path="/checkout/information"
+            component={InformationContainer}
+          />
+          <Route exact path="/checkout/payment" component={PaymentContainer} />
+          <Route exact path="/checkout/success" component={SuccessContainer} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
     </BrowserRouter>
   );
 };
