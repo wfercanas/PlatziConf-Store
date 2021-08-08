8caa8a2939852da64d428b200fcdcba615e3d7f2
wfercanas <fernando.canas@outlook.com>
2021-08-07 20:15:08 -0500

    add/ success mockup

--- src/containers/SuccessContainer/index.jsx
+++ src/containers/SuccessContainer/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import '../../styles/components/Success.css';
 
 const SuccessContainer = () => {
-  return;
+  return (
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Fernando, Gracias por tu compra</h2>
+        <span>Tu pedido llegará en 3 días a tu dirección.</span>
+        <div className="Success-map">Google Maps</div>
+      </div>
+    </div>
+  );
 };
 
 export { SuccessContainer };
