6e7c8ad0ebeb574f3c7afc3a5c7917dea18175c2
wfercanas <fernando.canas@outlook.com>
2021-08-07 20:10:37 -0500

    add/ Information mockup

--- src/containers/InformationContainer/index.jsx
+++ src/containers/InformationContainer/index.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import '../../styles/components/Information.css';
 
 const InformationContainer = () => {
-  return;
+  return (
+    <div className="Information">
+      <div className="Information-content">
+        <div className="Information-head">
+          <h2>Información de contacto:</h2>
+        </div>
+        <div className="Information-form">
+          <form>
+            <input type="text" placeholder="Nombre completo" name="name" />
+            <input type="email" placeholder="Correo electrónico" name="email" />
+            <input type="text" placeholder="Dirección" name="address" />
+            <input type="text" placeholder="Apto" name="apto" />
+            <input type="text" placeholder="Ciudad" name="city" />
+            <input type="text" placeholder="País" name="country" />
+            <input type="text" placeholder="Estado" name="state" />
+            <input type="text" placeholder="Código postal" name="zip-code" />
+            <input type="text" placeholder="Teléfono" name="phone" />
+          </form>
+        </div>
+        <div className="Information-buttons">
+          <div className="Information-back">Regresar</div>
+          <div className="Information-next">Pagar</div>
+        </div>
+      </div>
+      <div className="Information-sidebar">
+        <h3>Pedido:</h3>
+        <div className="Information-item">
+          <div className="Information-element">
+            <h4>Item name</h4>
+            <span>$10</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export { InformationContainer };
