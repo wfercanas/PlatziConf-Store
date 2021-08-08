3d4c064e4d2bd0eb31488ffb88a10983cd52566e
wfercanas <fernando.canas@outlook.com>
2021-08-07 20:00:59 -0500

    add/ checkout mockup

--- src/containers/CheckoutContainer/index.jsx
+++ src/containers/CheckoutContainer/index.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import '../../styles/components/Checkout.css';
 
 const CheckoutContainer = () => {
-  return;
+  return (
+    <div className="Checkout">
+      <div className="Checkout-content">
+        <h3>Lista de pedidos:</h3>
+        <div className="Checkout-item">
+          <div className="Checkout-element">
+            <h4>Item name</h4>
+            <span>$10</span>
+          </div>
+          <button type="button">Eliminar</button>
+        </div>
+      </div>
+      <div className="Checkout-sidebar">
+        <h3>Precio Total: $10</h3>
+        <button type="button">Continuar pedido</button>
+      </div>
+    </div>
+  );
 };
 
 export { CheckoutContainer };
