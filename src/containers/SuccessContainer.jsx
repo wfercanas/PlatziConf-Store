a9ce98ed3d09b3f7560d6e315d9f52fcfbaa1bb7
wfercanas <fernando.canas@outlook.com>
2021-08-09 22:36:05 -0500

    add/ fetch api with buyer address

--- src/containers/SuccessContainer.jsx
+++ src/containers/SuccessContainer.jsx
@@ -1,18 +1,22 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 import { Map } from '../components/Map';
+import { useGoogleAddress } from '../hooks/useGoogleAddress';
+
 import '../styles/components/Success.css';
 
 const SuccessContainer = () => {
   const { state } = useContext(AppContext);
   const { buyer } = state;
+  const location = useGoogleAddress(buyer[0]);
+
   return (
     <div className="Success">
       <div className="Success-content">
         <h2>{`${buyer[0].name}, Gracias por tu compra`}</h2>
         <span>Tu pedido llegará en 3 días a tu dirección.</span>
         <div className="Success-map">
-          <Map />
+          <Map location={location} />
         </div>
       </div>
     </div>
