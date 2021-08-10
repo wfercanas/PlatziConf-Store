b30c60cf426d110b1ccebf0a0a8c9bdb13de353e
wfercanas <fernando.canas@outlook.com>
2021-08-09 21:50:07 -0500

    add/ render buyer name and google map

--- src/containers/SuccessContainer.jsx
+++ src/containers/SuccessContainer.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+import { Map } from '../components/Map';
 import '../styles/components/Success.css';
 
 const SuccessContainer = () => {
+  const { state } = useContext(AppContext);
+  const { buyer } = state;
   return (
     <div className="Success">
       <div className="Success-content">
-        <h2>Fernando, Gracias por tu compra</h2>
+        <h2>{`${buyer[0].name}, Gracias por tu compra`}</h2>
         <span>Tu pedido llegará en 3 días a tu dirección.</span>
-        <div className="Success-map">Google Maps</div>
+        <div className="Success-map">
+          <Map />
+        </div>
       </div>
     </div>
   );
